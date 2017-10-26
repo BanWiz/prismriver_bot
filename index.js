98ed06ce6d0351eb21e9483ea664ebbaa28d7e5e
Yurii Kostiv <fobos1234321@gmail.com>
2017-10-26 07:10:29 +0300

    Metadata loading and now playing display

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const Discord = require('discord.js');
 const config = require('./config');
 const commands = require('./commands');
+const metadata = require('./metadata');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+  metadata.load();
   console.log('I am ready!');
+});
+
+metadata.on('update', data => {
+  var str = data.artist;
+  if(data.circle) {
+    str += '(' + data.circle + ')';
+  }
+  str += ' - ' + data.title;
+  client.user.setGame(str);
 });
 
 client.on('message', commands);
---
+++ metadata.js
@@ -0,0 +1,51 @@
+const EventEmitter = require('events');
+const https = require('https');
+const {parseString} = require('xml2js');
+
+class Metadata extends EventEmitter {
+    constructor() {
+        super();
+        this.metadata = {
+            albumart: '',
+            artist: '',
+            album: '',
+            circle: '',
+            year: ''
+        }
+    }
+    load() {
+        https.get('https://gensokyoradio.net/xml/', (res) => {
+            var buffer = '';
+            res.setEncoding('utf8');
+            res.on('data', chunk => buffer += chunk);
+            res.on('end', () => {
+                parseString(buffer, (err, result) => {
+                    var time = parseInt(result.GENSOKYORADIODATA.SONGTIMES[0].REMAINING[0], 10) * 1000  ;
+                    var songInfo = result.GENSOKYORADIODATA.SONGINFO[0];
+                    this.metadata.artist = songInfo.ARTIST[0];
+                    this.metadata.title = songInfo.TITLE[0];
+                    this.metadata.album = songInfo.ALBUM[0];
+                    this.metadata.year = songInfo.YEAR[0];
+                    this.metadata.circle = songInfo.CIRCLE[0];
+                    this.metadata.albumart = result.GENSOKYORADIODATA.MISC[0].ALBUMART[0] || "gr6_circular.png";
+
+                    console.log(this.metadata);
+
+                    setTimeout(() => this.load(), time);
+
+                    this.emit("update", this.metadata);
+                });
+            })
+        })
+    }
+}
+
+var metadata = {
+    albumart: '',
+    artist: '',
+    album: '',
+    circle: '',
+    year: ''
+}
+
+module.exports = new Metadata();
