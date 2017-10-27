112aec10f91ef6849c2c3b4b99f3b8e26c6f616d
Yurii Kostiv <fobos1234321@gmail.com>
2017-10-27 03:37:32 +0300

    Refactoring in preparations to prefixes update

--- commands.js
+++ commands.js
@@ -1,42 +1,61 @@
-var player = require('./player')
+var player = require('./player');
+
+var commands = {
+    play: function(message) {
+        if (!message.guild) {
+            message.reply('Sorry, I cannot play in private :frowning:');
+        } else if (!message.member.voiceChannel) {
+            message.reply('You are not in the voice channel');
+        } else {
+            player.addInstance(message.member.voiceChannel, (success) => {
+                if (success) {
+                    message.reply('Starting playing');
+                } else {
+                    message.reply("Looks like I'm already playing here");
+                }
+            })
+        }
+    },
+    stop: function(message) {
+        if (!message.guild) {
+            message.reply("I'm not playing in PM. I cannot...");
+        } else if (!player.getInstanceList()[message.guild.id]) {
+            message.reply("It looks like I don't play here. Cannot stop");
+        } else {
+            message.reply("Ok, stopping playing");
+            player.stopInstance(message.guild.id);
+        }
+    },
+    streams: function(message) {
+        var list = player.getInstanceList();
+        var num = 0;
+        for (var key in list) {
+            num++;
+        }
+        if (num) {
+            message.reply("I am currently playing on " + num + " server" + (num % 10 === 1 && num % 100 !== 11 ? "" : 's'));
+        } else {
+            message.reply("I don't play anywhere now");
+        }
+    }
+}
+
+function checkPrefix(message) {
+    const prefix = '.prismriver ';
+    if(!message.content.startsWith(prefix)) {
+        return false;
+    }
+    return message.content.substring(prefix.length);
+}
 
 module.exports = function (message) {
-    switch (message.content) {
-        case '.prismriver play':
-            if (!message.guild) {
-                message.reply('Sorry, I cannot play in private :frowning:');
-            } else if (!message.member.voiceChannel) {
-                message.reply('You are not in the voice channel');
-            } else {
-                player.addInstance(message.member.voiceChannel, (success) => {
-                    if (success) {
-                        message.reply('Starting playing');
-                    } else {
-                        message.reply("Looks like I'm already playing here");
-                    }
-                })
-            }
-            break;
-        case '.prismriver stop':
-            if (!message.guild) {
-                message.reply("I'm not playing in PM. I cannot...");
-            } else if (!player.getInstanceList()[message.guild.id]) {
-                message.reply("It looks like I don't play here. Cannot stop");
-            } else {
-                message.reply("Ok, stopping playing");
-                player.stopInstance(message.guild.id);
-            }
-            break;
-        case '.prismriver streams':
-            var list = player.getInstanceList();
-            var num = 0;
-            for (var key in list) {
-                num++;
-            }
-            if (num) {
-                message.reply("I am currently playing on " + num + " server" + (num % 10 === 1 && num % 100 !== 11 ? "" : 's'));
-            } else {
-                message.reply("I don't play anywhere now");
-            }
+    var params = checkPrefix(message);
+    if(!params) {
+        return;
+    }
+    params = params.split(' ');
+    var command = params.shift();
+    if(commands[command]) {
+        commands[command](message, params);
     }
 }
