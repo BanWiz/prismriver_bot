3080ff8b8e5a25a43344bd2599178d9f9db34fe1
BanWiz <fobos1234321@gmail.com>
2017-11-19 23:33:36 +0200

    Logging tracks

--- metadata.js
+++ metadata.js
@@ -21,9 +21,7 @@ class Metadata extends EventEmitter {
             res.on('end', () => {
                 parseString(buffer, (err, result) => {
                     var time = parseInt(result.GENSOKYORADIODATA.SONGTIMES[0].REMAINING[0], 10) * 1000;
-                    if(time <= 0) {
-                        time = 1000;
-                    }
+                    
                     var songInfo = result.GENSOKYORADIODATA.SONGINFO[0];
                     this.metadata.artist = songInfo.ARTIST[0];
                     this.metadata.title = songInfo.TITLE[0];
@@ -31,6 +29,14 @@ class Metadata extends EventEmitter {
                     this.metadata.year = songInfo.YEAR[0];
                     this.metadata.circle = songInfo.CIRCLE[0];
                     this.metadata.albumart = result.GENSOKYORADIODATA.MISC[0].ALBUMART[0];
+
+                    console.log("Playing " + this.metadata.artist + " - " + this.metadata.title);
+
+                    console.log("Remaining time: " + time);
+                    if(time <= 0) {
+                        console.log("Changing to 1 sec");
+                        time = 1000;
+                    }
 
                     setTimeout(() => this.load(), time);
 
